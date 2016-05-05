06c05def650ebc7d611b5686e1e179954a286f5c
seatbelts <is.ma95@hotmail.com>
2016-05-05 12:39:52 -0500

    fix

--- app/modules/maestros/controllers/CrearMaestrosController.js
+++ app/modules/maestros/controllers/CrearMaestrosController.js
@@ -1,11 +1,25 @@
 angular.module('its.maestros')
 	.controller('CrearMaestrosController', ['$scope', '$rootScope', 'toaster', 'APIServices', function($scope, $rootScope, toaster, APIServices){
 		$scope.maestro = {};
+		$scope.materias = {};
+
+		activate = function() {
+			APIServices.getSubject()
+                .then(function(res) {
+                    $scope.materias = res.data;
+                });
+		};
 
 		$scope.crear = function() {
-			APIServices.createTeacher($scope.maestro)
+			console.log('paso')
+			APIServices.createTeachers($scope.maestro)
 				.then(function(res) {
 					console.log(res);
+					if(res.status === 200) {
+						toaster.pop('success', 'Exito', 'Se creo al maestro')
+					}
 				})
-		}
-	}])
+		};
+
+		activate();
+	}]);
